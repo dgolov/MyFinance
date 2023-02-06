557b6cd903a229ede95c09fa48162ae0ede500f0
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-06 14:17:18 +0300

    Updated serializers

--- MyFinance/schemas.py
+++ MyFinance/schemas.py
@@ -2,6 +2,30 @@ from datetime import datetime
 from pydantic import BaseModel, Field, root_validator
 
 
+class Currency(BaseModel):
+    """ Валюта (рубли, доллары, евро и тд)
+    """
+    name: str
+
+
+class Account(BaseModel):
+    """ Счета
+    """
+    name: str
+    currency_id: Currency
+    updated_at: datetime = datetime.now()
+    amount: float
+    add_to_balance: bool = True
+
+    class Config:
+        validate_assignment = True
+
+    @root_validator
+    def update(cls, values):
+        values["updated_at"] = datetime.now()
+        return values
+
+
 class Category(BaseModel):
     """ Категория доходов и рассходов
     """
@@ -13,7 +37,8 @@ class BaseFinance(BaseModel):
     """ Базовый класс доходов и рассходов
     """
     title: str
-    category: Category
+    category_id: Category
+    account_id = Account
     amount: float = Field(..., gt=0)
     date: datetime = Field(default_factory=datetime.now)
 
@@ -28,27 +53,3 @@ class Expense(BaseFinance):
     """ Расходы
     """
     person: str = None
-
-
-class Currency(BaseModel):
-    """ Валюта (рубли, доллары, евро и тд)
-    """
-    name: str
-
-
-class Account(BaseModel):
-    """ Счета
-    """
-    name: str
-    currency: Currency
-    updated_at: datetime = datetime.now()
-    amount: float
-    add_to_balance: bool = True
-
-    class Config:
-        validate_assignment = True
-
-    @root_validator
-    def update(cls, values):
-        values["updated_at"] = datetime.now()
-        return values
