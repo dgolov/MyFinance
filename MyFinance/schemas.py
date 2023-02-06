13007a1989bfaedc30e365588ce1e31929906f16
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-07 00:11:49 +0300

    Updated schemas

--- MyFinance/schemas.py
+++ MyFinance/schemas.py
@@ -2,20 +2,46 @@ from datetime import datetime
 from pydantic import BaseModel, Field, root_validator
 
 
-class Currency(BaseModel):
-    """ Валюта (рубли, доллары, евро и тд)
-    """
+class CurrencyBase(BaseModel):
     name: str
 
+    class Config:
+        orm_mode = True
 
-class Account(BaseModel):
-    """ Счета
+
+class CurrencyList(CurrencyBase):
+    """ Список валют (рубли, доллары, евро и тд)
     """
+    id: int
+
+
+class CreateCurrency(CurrencyBase):
+    """ Создане валют (рубли, доллары, евро и тд)
+    """
+    pass
+
+
+class AccountBase(BaseModel):
     name: str
-    currency_id: Currency
-    updated_at: datetime = datetime.now()
+    updated_at: datetime
     amount: float
     add_to_balance: bool = True
+
+    class Config:
+        orm_mode = True
+
+
+class AccountList(AccountBase):
+    """ Список счетов
+    """
+    id: int
+    currency: CurrencyList
+
+
+class CreateAccount(AccountBase):
+    """ Создание счета
+    """
+    currency_id: int
 
     class Config:
         validate_assignment = True
@@ -26,35 +52,52 @@ class Account(BaseModel):
         return values
 
 
-class Category(BaseModel):
-    """ Категория доходов и рассходов
-    """
+class CategoryBase(BaseModel):
     name: str
     category_type: str
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryList(CategoryBase):
+    """ Список категорий доходов и рассходов
+    """
+    id: str
+
+
+class CreateCategory(CategoryBase):
+    """ Создание категорий доходов и рассходов
+    """
+    pass
 
 
 class BaseFinance(BaseModel):
     """ Базовый класс доходов и рассходов
     """
     title: str
-    category_id: Category
-    account_id = Account
     amount: float = Field(..., gt=0)
+    date: datetime = Field(default_factory=datetime.now)
+
+    class Config:
+        orm_mode = True
+
+
+class BaseFinanceList(BaseFinance):
+    """ Базовый класс списков доходов и рассходов
+    """
+    category: CategoryList
+    account = AccountList
+    id: int
 
 
 class IncomeList(BaseFinance):
     """ Список доходов
     """
-    date: datetime = Field(default_factory=datetime.now)
-
-    class Config:
-        orm_mode = True
+    pass
 
 
 class ExpenseList(BaseFinance):
     """ Список расходов
     """
-    date: datetime = Field(default_factory=datetime.now)
-
-    class Config:
-        orm_mode = True
+    pass
