a3f8680f807104f7ac3a2c4e8ac6a0f74298ef5c
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-06 14:15:09 +0300

    Updated migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,15 @@
-from alembic import context
+import sys
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from core.db import Base, SQLALCHEMY_DATABASE_URL
+from alembic import context
 
+
+sys.path = ['', '..'] + sys.path[1:]
+
+
+from core.engine import SQLALCHEMY_DATABASE_URL
+from core.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -38,8 +44,7 @@ def run_migrations_offline() -> None:
 
     """
     # url = config.get_main_option("sqlalchemy.url")
-    url = config.get_main_option(SQLALCHEMY_DATABASE_URL)
-
+    url = SQLALCHEMY_DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,15 +56,17 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration['sqlalchemy.url'] = SQLALCHEMY_DATABASE_URL
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
