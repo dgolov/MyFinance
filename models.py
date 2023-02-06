1ce73398766b897e992a847f04fe3c185e32a355
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-06 12:59:18 +0300

    Added docstring

--- models.py
+++ models.py
@@ -3,11 +3,15 @@ from pydantic import BaseModel, Field, root_validator
 
 
 class Category(BaseModel):
+    """ Категория доходов и рассходов
+    """
     name: str
     category_type: str
 
 
 class BaseFinance(BaseModel):
+    """ Базовый класс доходов и рассходов
+    """
     title: str
     category: Category
     amount: float = Field(..., gt=0)
@@ -15,18 +19,26 @@ class BaseFinance(BaseModel):
 
 
 class Income(BaseFinance):
+    """ Доходы
+    """
     company: str = None
 
 
 class Expense(BaseFinance):
+    """ Расходы
+    """
     person: str = None
 
 
 class Currency(BaseModel):
+    """ Валюта (рубли, доллары, евро и тд)
+    """
     name: str
 
 
 class Account(BaseModel):
+    """ Счета
+    """
     name: str
     currency: Currency
     updated_at: datetime = datetime.now()
