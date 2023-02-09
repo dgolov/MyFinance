14187ebe02ccbb316dfad3398ac8a0b0e167e949
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-10 01:21:25 +0300

    updated imports

--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, DateTime, Float, ForeignKey, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, DateTime
 
 Base = declarative_base()
 
