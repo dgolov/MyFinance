5b182ce2b8441a6a487f4316364fe6a7e9d3acdd
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-13 21:19:48 +0300

    Added utils

--- routes.py
+++ routes.py
@@ -1,20 +1,13 @@
 from fastapi import APIRouter
-from fastapi_users import FastAPIUsers, fastapi_users
 from MyFinance import finance
-from user.models import User
 from user.auth import auth_backend
-from user.manager import get_user_manager
 from user.schemas import UserRead, UserCreate
+from user.utils import fastapi_users
 
 routes = APIRouter()
 
 routes.include_router(finance.router, prefix="/finance")
 
-
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
 
 routes.include_router(
     fastapi_users.get_auth_router(auth_backend, requires_verification=True),
