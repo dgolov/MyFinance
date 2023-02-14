7fe9f62f5c909a9531f742e9c2a48430d13067cd
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-14 18:34:05 +0300

    Added verify and forgot password routes

--- routes.py
+++ routes.py
@@ -24,3 +24,15 @@ routes.include_router(
     prefix="/auth",
     tags=["auth"],
 )
+
+routes.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+routes.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     hashed_password: str = Column(String(length=1024), nullable=False)
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
-    is_verified: bool = Column(Boolean, default=True, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
